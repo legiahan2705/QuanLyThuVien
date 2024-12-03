fac69f5271f51e099696db8d196f928c73ba82f1
legiahan2705 <legiahancv@gmail.com>
2024-12-03 13:59:14 +0700

    Initial commit

---
+++ BL/TaiKhoanBL.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TO;
+using DL;
+
+namespace BL
+{
+    public class TaiKhoanBL
+    {
+        TaiKhoanAccess tkAccess = new TaiKhoanAccess();
+        public string CheckLogin(TaiKhoan_TO taikhoan)
+        {
+            // Kiểm tra nghiệp vụ
+            if (taikhoan.MaNV == "")
+            {
+                return "required_taikhoan";
+            }
+            if (taikhoan.MK == "")
+            {
+                return "required_matkhau";
+            }
+
+            string info = tkAccess.CheckLogin(taikhoan);
+            return info;
+        }
+    }
+}
---
+++ DL/TaiKhoanAccess.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TO;
+
+namespace DL
+{
+    public class TaiKhoanAccess : DataBaseAccess
+    {
+        public string CheckLogin(TaiKhoan_TO taikhoan)
+        {
+            string info = CheckLogin_TO(taikhoan);
+            return info;
+        }
+    }
+}
